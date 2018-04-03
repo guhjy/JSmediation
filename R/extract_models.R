856a756011533ba25eb8359f4ec490760c0dce11
Cedric Batailler <cedric.batailler@gmail.com>
2018-04-03 17:25:13 +0200

    Adding extract_tidy_models function

--- R/extract_models.R
+++ R/extract_models.R
@@ -18,3 +18,28 @@ extract_models <- function(mediation_model) {
 extract_models.mediation_model <- function(mediation_model) {
   mediation_model$js_models
 }
+
+#' Helper to extract tidy models from a mediation object
+#'
+#' With joint significant approach, different models are fitted to the data.
+#' This function helps you access the models that have been fit when using
+#' \code{mediation_model()}.
+#'
+#' @param mediation_model A mediation model fitted with \code{mediation_model}
+#'   method.
+#'
+#' @return A data.frame with models information.
+#'
+#' @export
+
+extract_tidy_models <- function(mediation_model) {
+  UseMethod("extract_tidy_models")
+}
+
+#' @export
+
+extract_tidy_models.mediation_model <- function(mediation_model) {
+  mediation_model$js_models %>%
+    purrr::map_df(~broom::tidy(.x), .id = "model")
+
+}
