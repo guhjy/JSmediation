8fcdcc975d69cb6c845cf4de68e1646a3aaf7817
Cedric Batailler <cedric.batailler@gmail.com>
2018-04-03 17:34:10 +0200

    adding mdt_moderated to fit moderated mediation models

---
+++ R/mdt_moderated.R
@@ -0,0 +1,67 @@
+#' Fit a moderated mediation model
+#'
+#' Given a dataframe, an IV, a DV, a mediator and a moderator, fit a moderated
+#'   mediation model.
+#'
+#' @param data dataframe
+#' @param IV IV
+#' @param Mediator Mediator
+#' @param DV DV
+#' @export
+
+mdt_moderated <- function(...) {
+  UseMethod("mdt_moderated")
+}
+
+#' @export
+mdt_moderated.data.frame <- function(data,
+                                     IV,
+                                     DV,
+                                     M,
+                                     Moderator) {
+
+  IV_var        <- enquo(IV)
+  DV_var        <- enquo(DV)
+  M_var         <- enquo(M)
+  Moderator_var <- enquo(Moderator)
+
+
+  model1 <-
+    stats::as.formula(glue::glue("{DV} ~ {IV} * {Mod}",
+                                 IV  = rlang::f_text(IV_var),
+                                 DV  = rlang::f_text(DV_var),
+                                 Mod = rlang::f_text(Moderator_var)))
+
+  model2 <-
+    stats::as.formula(glue::glue("{M} ~ {IV} * {Mod}",
+                                 IV  = rlang::f_text(IV_var),
+                                 M   = rlang::f_text(M_var),
+                                 Mod = rlang::f_text(Moderator_var)))
+
+  model3 <-
+    stats::as.formula(glue::glue("{DV} ~ ({IV} + {M}) * {Mod}",
+                                 DV  = rlang::f_text(DV_var),
+                                 IV  = rlang::f_text(IV_var),
+                                 M   = rlang::f_text(M_var),
+                                 Mod = rlang::f_text(Moderator_var)))
+
+  mediation_model <-
+    tibble::lst(
+      type      = "moderated mediation",
+      method    = "Joint significant",
+      model     = list("IV" = rlang::f_text(IV_var),
+                       "DV" = rlang::f_text(DV_var),
+                       "M"  = rlang::f_text(M_var),
+                       "Moderator" = rlang::f_text(Moderator_var)),
+      CI        = FALSE,
+      js_models =
+        list("X * Mod -> Y"     = model1,
+             "X * Mod -> M"     = model2,
+             "(X + M) * Mod -> Y" = model3) %>%
+        purrr::map(~lm(.x, data)),
+      js_models_summary =
+        purrr::map(js_models, ~broom::tidy(.x))
+    )
+
+  as_mediation_model(mediation_model)
+}
