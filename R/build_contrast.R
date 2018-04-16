565301d9a1bcc2a1fbfa1e7b935c4a2a579623a9
Cedric Batailler <cedric.batailler@hotmail.com>
2018-04-16 17:25:37 +0200

    build_contrast doc

--- R/build_contrast.R
+++ R/build_contrast.R
@@ -1,9 +1,22 @@
 #' @title build a contrast code from character vector
 #'
-#' @param vector a character vector.
-#' @param cond_a a character string to be coded -0.5
-#' @param cond_b a character string to be coded  0.5
+#' @description This helper function helps you build a contrast code from a
+#'   character variable.
 #'
+#' @param vector A character vector.
+#' @param cond_a A character string to be coded -0.5
+#' @param cond_b A character string to be coded  0.5
+#'
+#' @return A numeric vector.
+#'
+#' @examples
+#'   data(linkedfate)
+#'
+#'   linkedfate$condition_contrast <- build_contrast(linkedfate$condition,
+#'                                                   "Low discrimination",
+#'                                                   "High discrimination")
+#'
+#'  head(linkedfate)
 #' @export
 build_contrast <- function(vector, cond_a, cond_b) {
   UseMethod("build_contrast")
