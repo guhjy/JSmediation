66be7a27aab547ddb2f9e8ba744be47a4d51d0dd
Cedric Batailler <cedric.batailler@hotmail.com>
2018-04-17 13:53:12 +0200

    mdt_simple documentation

--- R/mdt_simple.R
+++ R/mdt_simple.R
@@ -1,11 +1,39 @@
 #' @title Simple mediation model
 #'
 #' @description
-#' \code{mdt_simple} fits a simple mediation model using joint significance
-#' method (Judd, Yzerbyt, & Muller, 2014).
+#' Given a dataframe, an IV (\code{IV}), a DV (\code{DV}) and a mediator
+#' (\code{M}), \code{mdt_simple} fits a simple mediation model using joint
+#' significance method (Judd, Yzerbyt, & Muller, 2014).
 #'
 #' @details
-#' Given a dataframe, an IV, a DV and a mediator, fits a mediation model.
+#' In a simple mediation analysis, researchers are intrested in finding if the
+#' effect of \eqn{X} on \eqn{Y} involve a third variable \eqn{M}. The basic
+#' hypothesis behind this test is that \eqn{X} has an effect on \eqn{M} which
+#' has an effect on \eqn{Y}: meaning that \eqn{X} has an indrect effect on
+#' \eqn{Y} through \eqn{M}.
+#'
+#' Total effect of \eqn{X} on \eqn{Y} can be described the following way:
+#'
+#' \deqn{c = c' + ab}
+#'
+#' where \eqn{c} is the total effect of \eqn{X} on \eqn{Y}, \eqn{c'} is the
+#' direct of \eqn{X} on \eqn{Y}, and \eqn{ab} is the indirect effect of \eqn{X}
+#' on \eqn{Y} through {M}.
+#'
+#' To asses if the indirect effect is different from the null, one has to
+#' asses the significance against the null for both \eqn{a} (the effect of
+#' \eqn{X} on \eqn{M}) for and \eqn{b} (effect of \eqn{M} on \eqn{Y} controlling
+#' for the effect of \eqn{X}) through the following set of linear models:
+#'
+#' \deqn{Y_i = \beta_{01} + c X_i}{Yi = \beta_01 + c*Xi}
+#' \deqn{M_i = \beta_{02} + a X_i}{Mi = \beta_02 + a*Xi}
+#' \deqn{Y_i = \beta_{03} + c' X_i + b M_i}{Yi = \beta_03 + c'*Xi + b*Mi}
+#'
+#' In joint significance mediation analysis approach, bot \eqn{a} and \eqn{b}
+#' must be significantly different from the null to conclude on the
+#' significance of the indirect effect.
+#'
+#' @family mediation models
 #'
 #' @references
 #' Judd, C. M., Yzerbyt, V. Y., & Muller, D. (2014). Mediation
@@ -13,15 +41,19 @@
 #' social and personality psychology, 2, 653–676.
 #'
 #' @param data a dataframe containing the variables in the model.
-#' @param IV an unquoted variable in the data frame which will be used
+#' @param IV an unquoted numeric variable in the data frame which will be used
 #'             as independant variable.
-#' @param M an unquoted variable in the data frame which will be used
+#' @param M an unquoted numeric variable in the data frame which will be used
 #'             as mediator.
-#' @param DV an unquoted variable in the data frame which will be used
+#' @param DV an unquoted numeric variable in the data frame which will be used
 #'             as dependant variable.
 #'
-#' @family mediation models
+#' @template mediation_model
 #'
+#' @examples
+#' \dontrun{
+#' mdt_simple(data = dataset, IV = X, DV = Y, M = Mediator)
+#' }
 #' @export
 
 mdt_simple <- function(data, IV, DV, M) {
