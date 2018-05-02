48e608da058e455fafcd6176b679f0112688c3a7
Cedric Batailler <cedric.batailler@hotmail.com>
2018-05-02 18:22:52 +0200

    Add mdt_within_2

---
+++ R/mdt_within2.R
@@ -0,0 +1,101 @@
+#' Fit a within-subject mediation model
+#'
+#' @param data a dataframe containing the variables in the model.
+#' @param DV_A
+#' @param DV_B
+#' @param M_A
+#' @param M_B
+#'
+#' @export
+mdt_within2 <- function(data, DV_A, DV_B, M_A, M_B) {
+  UseMethod("mdt_within2")
+}
+
+#' @export
+mdt_within2.data.frame <- function(data, DV_A, DV_B, M_A, M_B) {
+
+  # nse -----------------------------------------------------------------------
+  DV_A_var       <- enquo(DV_A)
+  DV_B_var       <- enquo(DV_B)
+  M_A_var        <- enquo(M_A)
+  M_B_var        <- enquo(M_B)
+
+  DV_A_name       <- rlang::quo_name(DV_A_var)
+  DV_B_name       <- rlang::quo_name(DV_B_var)
+  M_A_name        <- rlang::quo_name(M_A_var)
+  M_B_name        <- rlang::quo_name(M_B_var)
+
+  DV_A_data       <- data %>% dplyr::pull( !! DV_A_var )
+  DV_B_data       <- data %>% dplyr::pull( !! DV_B_var )
+  M_A_data        <- data %>% dplyr::pull( !! M_A_var )
+  M_B_data        <- data %>% dplyr::pull( !! M_B_var )
+
+  # type check ----------------------------------------------------------------
+
+  if(!is.numeric(DV_A_data))
+    stop(glue::glue("Warning:
+                    Mediator ({DV_A_name}) must be numeric."))
+
+  if(!is.numeric(DV_B_data))
+    stop(glue::glue("Warning:
+                    Mediator ({DV_B_name}) must be numeric."))
+
+  if(!is.numeric(M_A_data))
+    stop(glue::glue("Warning:
+                    Mediator ({M_A_name}) must be numeric."))
+
+  if(!is.numeric(M_B_data))
+    stop(glue::glue("Warning:
+                    Mediator ({M_B_name}) must be numeric."))
+
+  # data wrangling ------------------------------------------------------------
+  dataset <-
+    data %>%
+    dplyr::mutate(M_diff  = !!M_A_Var - !!M_V_Var ,
+                  M_mean  = (!!M_A_Var + !!M_V_Var) / 2,
+                  M_mean_center = scale(m_mean, scale = FALSE),
+                  DV_diff = !!DV_A_Var - !!DV_B_Var)
+
+  # bulding models ------------------------------------------------------------
+  model1 <-
+    stats::as.formula(DV_diff ~ 1)
+
+  model2 <-
+    stats::as.formula(M_diff ~ 1)
+
+  model3 <-
+    stats::as.formula(DV_diff ~ 1 + M_diff + M_mean_center)
+
+  # model fitting and cleaning ------------------------------------------------
+  js_models <-
+    list("1 -> DV_diff"                   = model1,
+         "1 -> M_diff"                    = model2,
+         "1 + M_diff + M_mean -> DV_diff" = model3) %>%
+    purrr::map(~lm(.x, dataset))
+
+  # paths ---------------------------------------------------------------------
+  paths <-
+    list("a"  = create_path(js_models, "1 -> M_diff", "(Intercept)"),
+         "b"  = create_path(js_models, "1 + M_diff + M_mean -> DV_diff", M_diff_name),
+         "c"  = create_path(js_models, "1 -> DV_diff", "(Intercept)"),
+         "c'" = create_path(js_models, "1 + M_diff + M_mean -> DV_diff", "(Intercept)"))
+
+  # bulding mediation model object --------------------------------------------
+  mediation_model <-
+    structure(
+      list(
+        type           = "within-mediation",
+        method         = "joint significant",
+        params         = list("DV_A" = DV_A_name,
+                              "DV_B" = DV_B_name,
+                              "M_A"  = M_A_name,
+                              "M_B"  = M_B_name),
+        paths          = paths,
+        indirect_index = FALSE,
+        js_models      = js_models,
+      ),
+      class = c("within_subject_mediation", "mediation_model")
+    )
+
+  mediation_model
+}
