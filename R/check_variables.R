ff981d020530908fcb25ad660a2b38f84d85d3a9
Cedric Batailler <cedric.batailler@hotmail.com>
2018-05-22 11:39:59 +0200

    tidyverse style guide

--- R/check_variables.R
+++ R/check_variables.R
@@ -17,32 +17,34 @@ check_variables.default <- function(model) {
 # Moderator is either a contrast-coded or a centered variable. Throws a
 # message if it is not the cas.
 check_variables.moderated_mediation <- function(model) {
-  IV_n  <- purrr::pluck(model, "params", "IV")
-  M_n   <- purrr::pluck(model, "params", "M")
+  IV_n <- purrr::pluck(model, "params", "IV")
+  M_n <- purrr::pluck(model, "params", "M")
   Mod_n <- purrr::pluck(model, "params", "Mod")
 
   Var_n <-
-    list(IV_n,
-       M_n,
-       Mod_n)
+    list(
+      IV_n,
+      M_n,
+      Mod_n
+    )
 
   Var_check <-
     Var_n %>%
-    purrr::map(~access_data(model, .x)) %>%
-    purrr::map_lgl(~(is_centered(.x) | is_contrast(.x)))
+    purrr::map(~ access_data(model, .x)) %>%
+    purrr::map_lgl(~ (is_centered(.x) | is_contrast(.x)))
 
   Var_n_check <-
     Var_n[!Var_check]
 
-  if(length(Var_n_check) != 0) {
+  if (length(Var_n_check) != 0) {
     message("\nMessage:")
     message(
-"It appears that the following variables are not
+      "It appears that the following variables are not
 contrast-coded or centred, please make sure it
 is intended as regression coefficients depends on
 the variables' coding:"
     )
-    for(var in Var_n_check) {
+    for (var in Var_n_check) {
       message(glue::glue("- {var}"))
     }
   }
