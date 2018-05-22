7c658054ede69dfa07bb2e3f85a7b40d7c6ecbba
Cedric Batailler <cedric.batailler@hotmail.com>
2018-05-22 11:44:24 +0200

    document is_centered and is_contrast

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,17 @@
+# Checks if the input can be considered as a contrast-coded vector.
+# Args:
+#   x: a numeric vector
+# Returns:
+#   A boolean.
 is_contrast <- function(x) {
   length(unique(x)) == 2L & sum(unique(x)) == 0
 }
 
+# Checks if the input can be considered as a centered vector.
+# Args:
+#   x: a numeric vector
+# Returns:
+#   A boolean.
 is_centered <- function(x) {
   # implementation to deal with floating number rounding error
   isTRUE(all.equal(mean(x), 0))
