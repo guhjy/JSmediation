02d3009020601aee44619ff966cdad6b938715ac
Cedric Batailler <cedric.batailler@hotmail.com>
2018-05-22 11:42:15 +0200

    Update JSmediation-package doc

--- R/JSmediation.R
+++ R/JSmediation.R
@@ -1,5 +1,26 @@
 #' @title JSmediation
 #'
-#' @description A set of helper functions to conduct mediation alaysis using
-#' the joint significance approach and tidy eval.
-"_PACKAGE"
+#' @name JSmediation-package
+#' @aliases JSmediation
+#'
+#' @description A set of helper functions to conduct joint significant test for
+#' mediation analysis.
+#'
+#' @details
+#' The main functions of the \pkg{JSmediation} package follow an
+#' \code{mdt_*} pattern and allow you to conduct a joint significant test. These
+#' function handle fitting the relevant linear model for different kind of
+#' mediation model.
+#'
+#' The syntax for \code{mdt_*} family functions is usually the same. The first
+#' argument is always a \code{data frame} (\code{data})  which is followed by
+#' the variable names involved in the model (e.g., DV, IV). Because
+#' \code{mdt_*} family functions use non-standard evaluation, these variable
+#' names must generaly be specified unquoted.
+#'
+#' \code{mdt_*} functions allow you to create an object of class
+#' \code{mediation_model} for which various methods are implemented.
+#'
+#' See \code{vignette("jsmediation")} for a general introduction and overview
+#' of \pkg{JSmediation}.
+NULL
