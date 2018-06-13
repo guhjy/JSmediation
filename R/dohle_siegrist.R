8d0e4226e8439dc4fb4c4b08e05f649d1f26137a
Cedric Batailler <cedric.batailler@hotmail.com>
2018-06-13 10:48:29 +0200

    Add dohle_siegrist data set

---
+++ R/dohle_siegrist.R
@@ -0,0 +1,21 @@
+#' @title Data set showing within-subject analysis
+#'
+#' @description A data set containing data from Experiment 1 from Dohle and
+#'   Siegrist (2014).
+#'
+#' @format A data frame with 44 rows and 4 variables:
+#' \describe{
+#'   \item{subject}{Subject number.}
+#'   \item{name}{Name of the drugs ("simple" vs. "complex").}
+#'   \item{hazardousness}{Mean estimated hazardousness.}
+#'   \item{willingness}{Mean willigness to buy.}
+#' }
+#'
+#' @references Dohle, S., & Siegrist, M. (2014). Fluency of pharmaceutical drug
+#' names predicts perceived hazardousness, assumed side effects and willingness
+#' to buy. \emph{Journal of Health Psychology}, \emph{19}(10), 1241‑1249.
+#' doi: 10.1177/1359105313488974
+
+
+#' @usage data("dohle_siegrist")
+"dohle_siegrist"
