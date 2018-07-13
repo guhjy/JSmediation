cd9d84ba3601bb9585fb1b13342b7e449627cb04
Cedric Batailler <cedric.batailler@hotmail.com>
2018-07-13 14:50:18 +0200

    Documentation - Update mdt_simple doc

--- R/mdt_simple.R
+++ R/mdt_simple.R
@@ -1,59 +1,84 @@
 #' @title Simple mediation model
 #'
-#' @description
-#' Given a data frame, an IV (\code{IV}), a DV (\code{DV}) and a mediator
-#' (\code{M}), \code{mdt_simple} fits a simple mediation model using joint
-#' significance method (Judd, Yzerbyt, & Muller, 2014).
+#' @description Given a data frame, a predictor (\code{IV}), an outcome
+#'   (\code{DV}), and a mediator (\code{M}), conducts a joint-significant test 
+#'   for simple mediation (see Yzerbyt, Muller, Batailler, & Judd, 2018).
 #'
-#' @details
-#' In a simple mediation analysis, researchers are intrested in finding if the
-#' effect of \eqn{X} on \eqn{Y} involve a third variable \eqn{M}. The basic
-#' hypothesis behind this test is that \eqn{X} has an effect on \eqn{M} which
-#' has an effect on \eqn{Y}: meaning that \eqn{X} has an indrect effect on
-#' \eqn{Y} through \eqn{M}.
-#'
-#' Total effect of \eqn{X} on \eqn{Y} can be described the following way:
-#'
-#' \deqn{c = c' + ab}
-#'
-#' where \eqn{c} is the total effect of \eqn{X} on \eqn{Y}, \eqn{c'} is the
-#' direct of \eqn{X} on \eqn{Y}, and \eqn{ab} is the indirect effect of \eqn{X}
-#' on \eqn{Y} through {M}.
-#'
-#' To assess if the indirect effect is different from the null, one has to
-#' assess the significance against the null for both \eqn{a} (the effect of
-#' \eqn{X} on \eqn{M}) for and \eqn{b} (effect of \eqn{M} on \eqn{Y} controlling
-#' for the effect of \eqn{X}) through the following set of linear models:
-#'
-#' \deqn{Y_i = \beta_{01} + c X_i}{Yi = \beta_01 + c*Xi}
-#' \deqn{M_i = \beta_{02} + a X_i}{Mi = \beta_02 + a*Xi}
-#' \deqn{Y_i = \beta_{03} + c' X_i + b M_i}{Yi = \beta_03 + c'*Xi + b*Mi}
-#'
-#' In joint significance mediation analysis approach, bot \eqn{a} and \eqn{b}
-#' must be significantly different from the null to conclude on the
-#' significance of the indirect effect.
-#'
-#' @family mediation models
-#'
-#' @references
-#' Judd, C. M., Yzerbyt, V. Y., & Muller, D. (2014). Mediation
-#' and moderation. Handbook of research methods in
-#' social and personality psychology, 2, 653–676.
-#'
-#' @param data a data frame containing the variables in the model.
-#' @param IV an unquoted numeric variable in the data frame which will be used
-#'             as independant variable.
-#' @param M an unquoted numeric variable in the data frame which will be used
-#'             as mediator.
-#' @param DV an unquoted numeric variable in the data frame which will be used
-#'             as dependant variable.
+#' @param data A data frame containing the variables to be used in the model.
+#' @param IV An unquoted numeric variable in the data frame which will be used
+#'   as independant variable.
+#' @param M An unquoted numeric variable in the data frame which will be used as
+#'   mediator.
+#' @param DV An unquoted numeric variable in the data frame which will be used
+#'   as dependant variable.
 #'
 #' @template mediation_model
 #'
+#' @family mediation models
+#'
+#' @details With simple mediation analysis, one is intrested in finding if the
+#'   effect of \eqn{X} on \eqn{Y} goes through a third variable \eqn{M}. The
+#'   hypothesis behind this test is that \eqn{X} has an effect on \eqn{M}
+#'   (\eqn{a}) which has an effect on \eqn{Y} (\eqn{b}), meaning that \eqn{X}
+#'   has an indrect effect on \eqn{Y} through \eqn{M}.
+#'
+#'   Total effect of \eqn{X} on \eqn{Y} can be described the following way:
+#'
+#'   \deqn{c = c' + ab}
+#'
+#'   with \eqn{c} the total effect of \eqn{X} on \eqn{Y}, \eqn{c'} the direct of
+#'   \eqn{X} on \eqn{Y}, and \eqn{ab} the indirect effect of \eqn{X} on \eqn{Y}
+#'   through {M} (see Models section).
+#'
+#'   To assess if the indirect effect is different from the null, one has to
+#'   assess the significance against the null for both \eqn{a} (the effect of
+#'   \eqn{X} on \eqn{M}) for and \eqn{b} (effect of \eqn{M} on \eqn{Y}
+#'   controlling for the effect of \eqn{X}). Both \eqn{a} and \eqn{b} needs to  
+#'   be simultaneously significant for an indirect effect to be claimed (Baron &
+#'   Kenny, 1986; Judd & Kenny, 1981).
+#'
+#' @section Models: In a simple mediation model, three models will be used:
+#'
+#'   - \eqn{Y_i = b_{10} + \mathbf{c_{11}} X_i}{Yi = b_10 + c_11*Xi} 
+#'   - \eqn{M_i = b_{20} + \mathbf{a_{21}} X_i}{Mi = b_20 + a_21*Xi} 
+#'   - \eqn{Y_i = b_{30} + \mathbf{c'_{31}} X_i + \mathbf{b_{32}} M_i}{Yi =
+#'     b_30 + c'_31*Xi + b_32*Mi}
+#'
+#'   with \eqn{Y_i}{Yi}, outcome value for the \emph{i}th observation,
+#'   \eqn{X_i}{Xi}, predictor value for the \emph{i}th observation, and
+#'   \eqn{M_i}{Mi}, mediator value for the \emph{i}th observation (Baron &
+#'   Kenny, 1986; Judd & Kenny, 1981).
+#'
+#' @section Variable coding: Because joint-significance tests uses linear models
+#'   uneder the scene, variables involved in the model have to be numeric.
+#'   \code{mdt_simple} will throw an error if non-numeric variables are
+#'   specified in the model.
+#'
+#'   If you need to convert a dichotomous categorical variable to a numeric one,
+#'   please refer to \code{\link{build_contrast}} function.
+#'
+#' @references
+#'
+#' Baron, R. M., & Kenny, D. A. (1986). The moderator-mediator variable
+#' distinction in social psychological research: Conceptual, strategic, and
+#' statistical considerations. \emph{Journal of Personality and Social
+#' Psychology}, \emph{51}(6), 1173‑1182.doi:10.1037/0022-3514.51.6.1173
+#'
+#' Judd, C. M., & Kenny, D. A. (1981). Process Analysis: Estimating Mediation in
+#' Treatment Evaluations. \emph{Evaluation Review}, \emph{5}(5), 602‑619. doi:
+#' 10.1177/0193841X8100500502
+#'
 #' @examples
-#' \dontrun{
-#' my_model <- mdt_simple(data = dataset, IV = X, DV = Y, M = Mediator)
-#' }
+#' ## fit a simple mediation model
+#' data(ho_et_al)
+#' ho_et_al$condition_c <- build_contrast(ho_et_al$condition,
+#'                                        "Low discrimination",
+#'                                        "High discrimination")
+#' mdt_simple(data = ho_et_al,
+#'            IV = condition_c,
+#'            DV = hypodescent,
+#'            M = linkedfate)
+#'
 #' @export
 
 mdt_simple <- function(data, IV, DV, M) {
