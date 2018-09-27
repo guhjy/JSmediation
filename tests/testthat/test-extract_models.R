340c408e630afff96b5c1b02fb00670db62e4cfc
Cedric Batailler <cedric.batailler@hotmail.com>
2018-09-27 11:11:00 -0500

    more test unit

--- tests/testthat/test-extract_models.R
+++ tests/testthat/test-extract_models.R
@@ -1,6 +1,6 @@
 context("test-extract_models")
 
-test_that("extract_model does throw an error when `step` = NULL", {
+test_that("extract_model throws an error when `step` = NULL", {
   data(ho_et_al)
   
   ho_et_al$condition_c <- build_contrast(ho_et_al$condition,
@@ -26,4 +26,32 @@ test_that("extract_model does not throw an error when used as intented", {
                       M    = linkedfate)
   
   expect_silent(extract_model(model, 1))
+})
+
+test_that("extract_models does not throw an error when used as intented", {
+  data(ho_et_al)
+  
+  ho_et_al$condition_c <- build_contrast(ho_et_al$condition,
+                                         "Low discrimination",
+                                         "High discrimination")
+  model <- mdt_simple(data = ho_et_al,
+                      IV   = condition_c,
+                      DV   = hypodescent,
+                      M    = linkedfate)
+  
+  expect_silent(extract_models(model))
+})
+
+test_that("extract_models does not throw an error when used as intented", {
+  data(ho_et_al)
+  
+  ho_et_al$condition_c <- build_contrast(ho_et_al$condition,
+                                         "Low discrimination",
+                                         "High discrimination")
+  model <- mdt_simple(data = ho_et_al,
+                      IV   = condition_c,
+                      DV   = hypodescent,
+                      M    = linkedfate)
+  
+  expect_silent(extract_tidy_models(model))
 })
