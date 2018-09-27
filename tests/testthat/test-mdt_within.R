d6d2857b232bc076147bf004a01638bc4e35995e
Cedric Batailler <cedric.batailler@hotmail.com>
2018-09-27 11:52:41 -0500

    more test unit

--- tests/testthat/test-mdt_within.R
+++ tests/testthat/test-mdt_within.R
@@ -8,12 +8,25 @@ test_that("mdt_within does not throw error", {
                            participant))
 })
 
+test_that("add_index method for mdt_within does not throw error", {
+  model <- 
+    mdt_within(dohle_siegrist,
+               name,
+               willingness,
+               hazardousness,
+               participant)
+  
+  expect_silent(add_index(model))
+})
+
 test_that("print method for mdt_within does not throw error", {
-  expect_output(
-    print(mdt_within(dohle_siegrist,
-                     name,
-                     willingness,
-                     hazardousness,
-                     participant))
-  )
+  model <- 
+    mdt_within(dohle_siegrist,
+               name,
+               willingness,
+               hazardousness,
+               participant)
+  
+  expect_output(print(model))
+  expect_output(print(add_index(model)))
 })
