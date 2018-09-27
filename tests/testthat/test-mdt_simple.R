0fad3b78bc64e6495a9858505d32eb89b745c7b3
Cedric Batailler <cedric.batailler@hotmail.com>
2018-09-27 11:47:22 -0500

    more test unit

--- tests/testthat/test-mdt_simple.R
+++ tests/testthat/test-mdt_simple.R
@@ -13,15 +13,16 @@ test_that("mdt_simple does not throw an error", {
   )
 })
 
-test_that("mdt_simple method does not throw an error", {
+test_that("mdt_simple print method does not throw an error", {
   data(ho_et_al)
   ho_et_al$condition_c <- build_contrast(ho_et_al$condition,
                                          "Low discrimination",
                                          "High discrimination")
-  expect_output(
-    print(mdt_simple(data = ho_et_al,
-                     IV   = condition_c,
-                     DV   = hypodescent,
-                     M    = linkedfate))
-  )
+  model <- mdt_simple(data = ho_et_al,
+                      IV   = condition_c,
+                      DV   = hypodescent,
+                      M    = linkedfate)
+  
+  expect_output(print(model))
+  expect_output(print(add_index, model))
 })
